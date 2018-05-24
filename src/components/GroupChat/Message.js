d704dbb2db74ba725bc8333f5bf1c5dc23f00d08
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-05-24 12:23:33 -0700

    added styling to have logged in users own message shows from the otherside

--- src/components/GroupChat/Message.js
+++ src/components/GroupChat/Message.js
@@ -3,16 +3,16 @@ import corner from '../shared/media/Asset 3.svg'
 
 const Message = (props) => {
 
-    let stylingForSelf = props.self ? 'message-name-date myself' : 'message-name-date'
+    let stylingForSelf = props.self ? 'individual-message-container myself' : 'individual-message-container guest';
     
     
 
     return (
-        <div className='individual-message-container'>
+        <div className={stylingForSelf}>
             <div className='user-image-container-for-messages'>
                 <img src={props.picture} />
             </div>
-            <div className={stylingForSelf}>
+            <div className='message-name-date'>
                 <div className='corner'></div>
                 <div className='message-text'>
                     <p>{props.message}</p>
