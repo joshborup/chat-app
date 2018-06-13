0e703280d48e37da669b2c58af6976bb87b23c78
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-06-12 19:33:04 -0700

    started on redesign to not pidgeonhole myself

--- src/components/GroupChat/UsersLists.js
+++ src/components/GroupChat/UsersLists.js
@@ -1,25 +1,9 @@
 import React, {Component} from 'react';
-import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { CSVLink } from 'react-csv';
-import Save from '@material-ui/icons/Save';
-import Hidden from '@material-ui/core/Hidden';
-import Drawer from '@material-ui/core/Drawer';
-import Menu from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import { Snackbar } from '@material-ui/core';
-import StatusSnackBar from '../GroupChat/SnackBar'
 
 
-class ResponsiveDrawer extends React.Component {
-  
-    state = {
-    mobileOpen: false,
-  };
-
-  handleDrawerToggle = () => {
-    this.setState({ mobileOpen: !this.state.mobileOpen });
-  };
+class ResponsiveDrawer extends Component {
 
   render() {
 
@@ -47,56 +31,7 @@ class ResponsiveDrawer extends React.Component {
 
     return (
         <div className='drawer-container'>
-        <button className='hamburger-button' onClick={this.handleDrawerToggle}>&#x2630;</button>
-
-            <Hidden >
-            <Drawer
-                className='mobile-drawer-content'
-                variant="temporary"
-                anchor='right'
-                onClose={this.handleDrawerToggle}
-                open={this.state.mobileOpen}
-                ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-                }}
-            >
-                <div className='close-save-container'>
-                    <CSVLink className='download-convo-button' data={this.props.csvData}  target="_blank" >
-                        <Button title='Save chat conversation'  variant="raised" size="small">
-                            <Save />
-                                Save
-                        </Button>
-                    </CSVLink>
-                    <button className='close-button' onClick={this.handleDrawerToggle}>X</button>
-                    <Link to='/'>
-                    <button><AccountCircle/></button>
-                </Link>
-                </div>
-                {usersList}
-                <StatusSnackBar justJoined={this.props.justJoined} close={this.props.snackBarClose} open={this.props.snackBarOpen}/>
-            </Drawer>
-            </Hidden>
-            <Hidden>
-            <Drawer
-                className='desktop-drawer-content'
-                variant="permanent"
-                open
-            >
-            <div>
-                <CSVLink data={this.props.csvData}  target="_blank" >
-                    <Button title='Save chat conversation'  variant="raised" size="small">
-                        <Save />
-                            Save
-                    </Button>
-                </CSVLink>
-                <Link to='/'>
-                    <button><AccountCircle/></button>
-                    </Link>
-            </div>
-                {usersList}
-                <StatusSnackBar justJoined={this.props.justJoined} close={this.props.snackBarClose} open={this.props.snackBarOpen}/>
-            </Drawer>
-            </Hidden>
+            {usersList}
         </div>
     );
   }
--- src/components/shared/HeaderLinks.js
+++ src/components/shared/HeaderLinks.js
@@ -9,35 +9,11 @@ export default class HeaderLinks extends Component {
                     <li className='link-1'>
                         <Link to='/group/cool'>Chat</Link>
                     </li>
-
-                    <li className='link-2'>
-                        chat
-                    </li>
-
-                        <li className='link-3'>
-                        chat
-                    </li>
-
-                        <li className='link-4'>
-                        chat
-                    </li>
                 </ul>
 
                 <ul>
-                <li className='link-1'>
+                    <li className='link-1'>
                         <Link to='/'>profile</Link>
-                    </li>
-
-                    <li className='link-2'>
-                        profile
-                    </li>
-
-                    <li className='link-3'>
-                        profile
-                    </li>
-
-                    <li className='link-4'>
-                        profile
                     </li>
                 </ul>
             </div>
