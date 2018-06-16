e8de13400082fbe97177f03be4e11df851fe0025
joshborup <joshborup@gmail.com>
2018-06-16 15:25:29 -0700

    starting design on chatcontainer

--- src/components/Chat/ChatContainer.js
+++ src/components/Chat/ChatContainer.js
@@ -4,7 +4,7 @@ import ChatDisplay from './ChatDisplay';
 export default class ChatContainer extends Component {
     render() {
         return (
-            <div>
+            <div className='chat-container'>
                 <ChatDisplay />
             </div>
         );
--- src/components/Chat/ChatDisplay.js
+++ src/components/Chat/ChatDisplay.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './chat.css';
 
 const ChatDisplay = (props) => {
     return (
-        <div>
-            hey
+        <div className='chat-display'>
+           <h1>Select a room</h1>
+
+            <div className='create-room'>
+                <button>
+                    create room
+                </button>
+            </div>
+
+           <div className='rooms'>
+               <div className='group'>
+                    <h2>
+                        Group
+                    </h2>
+                </div>
+
+                <div className='random'>
+                    <h2>
+                        Random
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 };
